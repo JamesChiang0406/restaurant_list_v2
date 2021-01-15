dc9014975e9eba62f962f3542f35dd0ab5a28549
James_C <a9356070@gmail.com>
2021-01-15 16:58:42 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const home = require('./modules/home')
 const { route } = require('./modules/resFunc')
 const resFunc = require('./modules/resFunc')
 const search = require('./modules/search')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurant', resFunc)
 router.use('/create', resFunc)
 router.use('/search', search)
+router.use('/users', users)
 
 module.exports = router
