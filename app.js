b98ae74468b257f0146553ed684be7cf72f3e892
James_C <a9356070@gmail.com>
2021-01-19 18:30:54 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 // require package used
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverrid = require('method-override')
 const routes = require('./routes')
-require('./config/mongoose')
 const restaurantList = require('./restaurant.json')
-const app = express()
-const port = 3000
 const session = require('express-session')
 const usePassport = require('./config/passport')
 const flash = require('connect-flash')
+const app = express()
+const port = process.env.PORT
+require('./config/mongoose')
 
 // setting session and passport and flash
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI
 
 // connect to mongoDB and set state
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 const db = mongoose.connection
 
 db.on('error', () => {   // connect error
